5a02e904fb5d85e5634f69ade1d03da53b5d34c8
Rodrigo Pedra Brum <rodrigo.pedra@gmail.com>
2018-03-20 10:39:02 -0300

    Render numeric Strings as Strings

--- src/Transformers/NumericTransformer.php
+++ src/Transformers/NumericTransformer.php
@@ -11,7 +11,7 @@ class NumericTransformer implements Transformer
      */
     public function canTransform($value): bool
     {
-        return is_numeric($value);
+        return is_int($value) || is_float($value);
     }
 
     /**
