1b7f4031793b8722cbc08020d3a0ac0cc9c4b143
alexmanase <alexmanase@users.noreply.github.com>
2022-11-10 18:35:42 +0000

    Fix styling

--- tests/BladeTest.php
+++ tests/BladeTest.php
@@ -14,79 +14,75 @@ it('can render', function (mixed $parameter, string $expected) {
     ->with([
         'an array' => [
             'parameter' => fn () => ['key' => 'value'],
-            'expected' => '<script>window[\'js\'] = window[\'js\'] || {};window[\'js\'][\'key\'] = \'value\';</script>'
+            'expected' => '<script>window[\'js\'] = window[\'js\'] || {};window[\'js\'][\'key\'] = \'value\';</script>',
         ],
         'a boolean with value of `true`' => [
             'parameter' => fn () => ['boolean' => true],
-            'expected' => '<script>window[\'js\'] = window[\'js\'] || {};window[\'js\'][\'boolean\'] = true;</script>'
+            'expected' => '<script>window[\'js\'] = window[\'js\'] || {};window[\'js\'][\'boolean\'] = true;</script>',
         ],
         'a boolean with value of `false`' => [
             'parameter' => fn () => ['boolean' => false],
-            'expected' => '<script>window[\'js\'] = window[\'js\'] || {};window[\'js\'][\'boolean\'] = false;</script>'
+            'expected' => '<script>window[\'js\'] = window[\'js\'] || {};window[\'js\'][\'boolean\'] = false;</script>',
         ],
         'an integer' => [
             'parameter' => fn () => ['number' => 5],
-            'expected' => '<script>window[\'js\'] = window[\'js\'] || {};window[\'js\'][\'number\'] = 5;</script>'
+            'expected' => '<script>window[\'js\'] = window[\'js\'] || {};window[\'js\'][\'number\'] = 5;</script>',
         ],
         'an float' => [
             'parameter' => fn () => ['number' => 5.5],
-            'expected' => '<script>window[\'js\'] = window[\'js\'] || {};window[\'js\'][\'number\'] = 5.5;</script>'
+            'expected' => '<script>window[\'js\'] = window[\'js\'] || {};window[\'js\'][\'number\'] = 5.5;</script>',
         ],
         'null' => [
             'parameter' => fn () => ['nothing' => null],
-            'expected' => '<script>window[\'js\'] = window[\'js\'] || {};window[\'js\'][\'nothing\'] = null;</script>'
+            'expected' => '<script>window[\'js\'] = window[\'js\'] || {};window[\'js\'][\'nothing\'] = null;</script>',
         ],
         'a string with line breaks' => [
             'parameter' => fn () => ['string' => "This is\r\n a test"],
-            'expected' => '<script>window[\'js\'] = window[\'js\'] || {};window[\'js\'][\'string\'] = \'This is\\r\\n a test\';</script>'
+            'expected' => '<script>window[\'js\'] = window[\'js\'] || {};window[\'js\'][\'string\'] = \'This is\\r\\n a test\';</script>',
         ],
         'a numeric string as a string' => [
             'parameter' => fn () => ['socialSecurity' => '123456789'],
-            'expected' => '<script>window[\'js\'] = window[\'js\'] || {};window[\'js\'][\'socialSecurity\'] = \'123456789\';</script>'
+            'expected' => '<script>window[\'js\'] = window[\'js\'] || {};window[\'js\'][\'socialSecurity\'] = \'123456789\';</script>',
         ],
         'escapes tags in a string' => [
             'parameter' => fn () => ['string' => "This is a <tag>"],
-            'expected' => '<script>window[\'js\'] = window[\'js\'] || {};window[\'js\'][\'string\'] = \'This is a \<tag\>\';</script>'
+            'expected' => '<script>window[\'js\'] = window[\'js\'] || {};window[\'js\'][\'string\'] = \'This is a \<tag\>\';</script>',
         ],
         'arrayable objects' => [
-            'parameter' => fn () => new class() implements Arrayable
-            {
+            'parameter' => fn () => new class () implements Arrayable {
                 public function toArray()
                 {
                     return ['arrayableKey' => 'arrayableValue'];
                 }
             },
-            'expected' => '<script>window[\'js\'] = window[\'js\'] || {};window[\'js\'][\'arrayableKey\'] = \'arrayableValue\';</script>'
+            'expected' => '<script>window[\'js\'] = window[\'js\'] || {};window[\'js\'][\'arrayableKey\'] = \'arrayableValue\';</script>',
         ],
         'JSON serializable objects' => [
-            'parameter' => fn () => new class() implements JsonSerializable
-            {
+            'parameter' => fn () => new class () implements JsonSerializable {
                 public function jsonSerialize()
                 {
                     return ['jsonKey' => 'jsonValue'];
                 }
             },
-            'expected' =>  '<script>window[\'js\'] = window[\'js\'] || {};window[\'js\'][\'0\'] = {"jsonKey":"jsonValue"};</script>'
+            'expected' => '<script>window[\'js\'] = window[\'js\'] || {};window[\'js\'][\'0\'] = {"jsonKey":"jsonValue"};</script>',
         ],
         'an object that implements `toJson`' => [
-            'parameter' => fn () => new class()
-            {
+            'parameter' => fn () => new class () {
                 public function toJson()
                 {
                     return json_encode(['jsonKey' => 'jsonValue']);
                 }
             },
-            'expected' =>  '<script>window[\'js\'] = window[\'js\'] || {};window[\'js\'][\'0\'] = {"jsonKey":"jsonValue"};</script>'
+            'expected' => '<script>window[\'js\'] = window[\'js\'] || {};window[\'js\'][\'0\'] = {"jsonKey":"jsonValue"};</script>',
         ],
         'an object that implements `toString`' => [
-            'parameter' => fn () => new class()
-            {
+            'parameter' => fn () => new class () {
                 public function __toString()
                 {
                     return 'string';
                 }
             },
-            'expected' =>  '<script>window[\'js\'] = window[\'js\'] || {};window[\'js\'][\'0\'] = \'string\';</script>'
+            'expected' => '<script>window[\'js\'] = window[\'js\'] || {};window[\'js\'][\'0\'] = \'string\';</script>',
         ],
 
     ]);
